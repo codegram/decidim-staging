e5a0dc4bfe625283237decc2d72ba9ea041d47a5
Marc Riera Casals <mrc2407@gmail.com>
2018-04-20 14:57:14 +0200

    Add missing mgirations

---
+++ db/migrate/20180420125631_publish_participatory_spaces.decidim.rb
@@ -0,0 +1,27 @@
+# This migration comes from decidim (originally 20180420092713)
+# frozen_string_literal: true
+
+class PublishParticipatorySpaces < ActiveRecord::Migration[5.1]
+  class ParticipatorySpace < ApplicationRecord
+    self.table_name = :decidim_participatory_spaces
+  end
+
+  class Organization < ApplicationRecord
+    self.table_name = :decidim_organizations
+  end
+
+  def up
+    %w(participatory_processes assemblies).each do |space_name|
+      Organization.find_each do |organization|
+        space = ParticipatorySpace.find_or_create_by(
+          organization: organization,
+          manifest_name: space_name
+        )
+        space.activate!
+        space.publish!
+      end
+    end
+  end
+
+  def down; end
+end
