576d79b3f51c2690f58b2eac5a71662d7587c438
Oriol Gual <oriol@codegram.com>
2019-04-08 10:59:50 +0200

    Try to reduce the initial rows so Heroku doesn't complain

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ if ENV["HEROKU_APP_NAME"].present?
   ENV["SEED"] = "true"
 end
 Decidim.seed!
+Decidim::ActionLog.delete_all
+PaperTrail::Version.delete_all
+Decidim::Gamification::BadgeScore.delete_all
