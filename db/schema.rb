9cbbe9187b35f45229c10dda6fb2b56920396017
Oriol Gual <oriol@codegram.com>
2019-10-08 09:32:51 +0200

    Update decidim

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_153539) do
+ActiveRecord::Schema.define(version: 2019_10_08_072021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
