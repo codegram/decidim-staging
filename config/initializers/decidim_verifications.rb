cfc4c463484ab8283098ba0fb57ae03f25de3fdf
Agustí B.R <agusti@codegram.com>
2020-03-13 07:58:58 +0100

    setup verifications as renewable

--- config/initializers/decidim_verifications.rb
+++ config/initializers/decidim_verifications.rb
@@ -4,6 +4,8 @@ Decidim::Verifications.register_workflow(:dummy_authorization_handler) do |workf
   workflow.form = "DummyAuthorizationHandler"
   workflow.action_authorizer = "DummyAuthorizationHandler::DummyActionAuthorizer"
   workflow.expires_in = 1.month
+  workflow.renewable = true
+  workflow.time_between_renewals = 1.minute
 
   workflow.options do |options|
     options.attribute :postal_code, type: :string, default: "08001", required: false
@@ -13,7 +15,9 @@ end
 Decidim::Verifications.register_workflow(:another_dummy_authorization_handler) do |workflow|
   workflow.form = "AnotherDummyAuthorizationHandler"
   workflow.expires_in = 1.month
-
+  workflow.renewable = true
+  workflow.time_between_renewals = 1.minute
+  
   workflow.options do |options|
     options.attribute :passport_number, type: :string, required: false
   end
