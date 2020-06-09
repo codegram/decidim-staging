aaaed828a6796f59f684e24a7148433055c9fd47
Gladys Chacartegui <gladys@codegram.com>
2020-06-09 16:05:43 +0200

    Update decidim with PR #6152

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_133961) do
+ActiveRecord::Schema.define(version: 2020_06_09_140202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -688,6 +688,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_133961) do
     t.bigint "decidim_component_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
     t.index ["decidim_component_id"], name: "index_decidim_elections_elections_on_decidim_component_id"
   end
 
