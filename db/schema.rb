f6488d1240abb04df271389358d7ad1d87fc7665
slickepinne <svenjaschaefer@gmail.com>
2020-08-14 15:13:07 +0200

    update with decidim #6390 and #6386

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_161454) do
+ActiveRecord::Schema.define(version: 2020_08_14_131136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
