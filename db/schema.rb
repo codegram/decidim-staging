cbd53c94125779aad742ad93b05eb3eb3c409fab
agustibr <agustibr@users.noreply.github.com>
2020-10-05 09:21:44 +0000

    Update decidim to feature/election-feedback branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_051458) do
+ActiveRecord::Schema.define(version: 2020_10_05_092142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
