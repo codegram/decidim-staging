90c4ac729475e011de794b1764e66d38988a3ee6
Oriol Gual <oriol@codegram.com>
2021-01-14 12:25:04 +0100

    Fix Decidim update

--- update_decidim.sh
+++ update_decidim.sh
@@ -1,1 +1,1 @@
-bundle update decidim decidim-dev decidim-initiatives && rake decidim:upgrade && rake db:migrate
+bundle update decidim decidim-dev decidim-initiatives decidim-conferences decidim-consultations decidim-elections decidim-templates && rake decidim:upgrade && rake db:migrate
