16763d6a9db83b1ef9d0da68adc57c15125205b4
Oriol Gual <oriol@codegram.com>
2021-01-19 11:52:41 +0100

    Update carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Default CarrierWave setup.
 #
 CarrierWave.configure do |config|
@@ -8,22 +9,25 @@ CarrierWave.configure do |config|
   config.enable_processing = !Rails.env.test?
 end
 
+# Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
+#
 if Rails.application.secrets.aws_access_key_id.present?
   require "carrierwave/storage/fog"
-
   CarrierWave.configure do |config|
-    config.root = Rails.root.join('tmp')
-    config.cache_dir = 'carrierwave'
     config.storage = :fog
     config.fog_provider = 'fog/aws'                                             # required
     config.fog_credentials = {
       provider:              'AWS',                                             # required
       aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-      region:                'eu-central-1',                                       # optional, defaults to 'us-east-1'
-      host:                  's3.eu-central-1.amazonaws.com',                                  # optional, defaults to nil
+      region:                Rails.application.secrets.aws_region,
+      host:                  Rails.application.secrets.aws_host,
     }
-    config.fog_directory  = Rails.application.secrets.aws_bucket              # required
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
+    config.fog_directory  = Rails.application.secrets.aws_bucket
+    config.fog_public = true
+    config.fog_attributes = {
+      'Cache-Control' => "max-age=#{365.day.to_i}",
+      'X-Content-Type-Options' => "nosniff"
+    }
   end
 end
