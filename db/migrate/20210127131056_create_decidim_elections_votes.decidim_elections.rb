c9657df01985b555004aceefcd14349709879d15
mrcasals <mrcasals@users.noreply.github.com>
2021-01-27 13:10:56 +0000

    Update decidim to feature/import_proposals_spreadsheet branch

---
+++ db/migrate/20210127131056_create_decidim_elections_votes.decidim_elections.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+# This migration comes from decidim_elections (originally 20201209110653)
+
+class CreateDecidimElectionsVotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :decidim_elections_votes do |t|
+      t.belongs_to :decidim_elections_election, null: false, index: { name: "index_elections_votes_on_decidim_elections_election_id" }
+      t.string :voter_id, null: false
+      t.string :encrypted_vote_hash, null: false
+      t.string :status, null: false
+
+      t.timestamps
+    end
+  end
+end
