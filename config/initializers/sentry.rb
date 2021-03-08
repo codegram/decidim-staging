09f533043357df3c612b664babdd4c80bc5bc3b8
Marc Riera Casals <mrc2407@gmail.com>
2021-03-08 15:48:08 +0100

    Fix Sentry env name

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,11 @@
 if Rails.application.secrets.sentry_enabled?
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
-    config.environment = ENV["HEROKU_APP_NAME"] if ENV["HEROKU_APP_NAME"].present?
+    config.environment = if ENV["HEROKU_APP_NAME"].present?
+                           ENV["HEROKU_APP_NAME"]
+                         else
+                           "main"
+                         end
 
     config.breadcrumbs_logger = [:active_support_logger]
 
