8f402485140b5e168d4d4bc306f12b25d3eac87d
Agusti B.R <agustibr@users.noreply.github.com>
2021-03-15 15:50:09 +0100

    Update 20201009122749_add_commentable_counter_cache_to_initiatives (#321)

--- db/migrate/20201009122749_add_commentable_counter_cache_to_initiatives.decidim_initiatives.rb
+++ db/migrate/20201009122749_add_commentable_counter_cache_to_initiatives.decidim_initiatives.rb
@@ -3,7 +3,7 @@
 
 class AddCommentableCounterCacheToInitiatives < ActiveRecord::Migration[5.2]
   def change
-    if defined?(Decidim::Consultations)
+    if defined?(Decidim::Initiatives)
       add_column :decidim_initiatives, :comments_count, :integer, null: false, default: 0, index: true
       Decidim::Initiative.reset_column_information
       Decidim::Initiative.find_each(&:update_comments_count)
