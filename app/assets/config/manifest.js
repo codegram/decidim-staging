44450138dcfea5dbf7a9a82ace6b4a46aa604118
David Morcillo <david@codegram.com>
2021-03-30 18:12:26 +0200

    chore: add missing assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,9 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+//= link decidim/elections/voter/casting-vote.js
+//= link decidim/elections/voter/new-vote.js
+//= link decidim/elections/voter/setup-preview.js
+//= link decidim/elections/voter/setup-vote.js
+//= link decidim/elections/voter/verify-vote.js
