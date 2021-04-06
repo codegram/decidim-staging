c7132efe9f36e427f6a34672b8b59af1136c19a9
Agustí B.R <hola@agusti.cat>
2021-04-06 19:50:39 +0200

    fix consultations migration

--- db/migrate/20210322053361_add_followable_counter_cache_to_consultations.decidim_consultations.rb
+++ db/migrate/20210322053361_add_followable_counter_cache_to_consultations.decidim_consultations.rb
@@ -3,13 +3,15 @@
 
 class AddFollowableCounterCacheToConsultations < ActiveRecord::Migration[5.2]
   def change
-    add_column :decidim_consultations_questions, :follows_count, :integer, null: false, default: 0, index: true
+    if defined?(Decidim::Consultations)
+      add_column :decidim_consultations_questions, :follows_count, :integer, null: false, default: 0, index: true
 
-    reversible do |dir|
-      dir.up do
-        Decidim::Consultations::Question.reset_column_information
-        Decidim::Consultations::Question.find_each do |record|
-          record.class.reset_counters(record.id, :follows)
+      reversible do |dir|
+        dir.up do
+          Decidim::Consultations::Question.reset_column_information
+          Decidim::Consultations::Question.find_each do |record|
+            record.class.reset_counters(record.id, :follows)
+          end
         end
       end
     end
