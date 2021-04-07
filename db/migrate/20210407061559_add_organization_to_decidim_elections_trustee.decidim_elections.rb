a924963378a7957b49f8b3aa760696c05642f73f
Agustí B.R <hola@agusti.cat>
2021-04-07 08:17:31 +0200

    update decidim

---
+++ db/migrate/20210407061559_add_organization_to_decidim_elections_trustee.decidim_elections.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# This migration comes from decidim_elections (originally 20210401095507)
+
+class AddOrganizationToDecidimElectionsTrustee < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :decidim_elections_trustees, :decidim_organization, index: true, foreign_key: true
+    add_index :decidim_elections_trustees,
+              [:name, :decidim_organization_id],
+              unique: true,
+              name: "index_decidim_organization_id_and_name"
+  end
+end
