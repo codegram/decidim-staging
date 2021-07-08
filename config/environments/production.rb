5d6fdd348ffea0df88157218f06d79215cc7360a
Oriol Gual <oriol@codegram.com>
2021-07-08 20:46:53 +0200

    Use inline queue adapter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  config.active_job.queue_adapter     = :async
+  config.active_job.queue_adapter     = :inline
   # config.active_job.queue_name_prefix = "decidim_staging_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
