cdf2b948d2589e39754a7790dec525d02c095e2e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2022-05-01 16:20:18 +0200

    Fix migrations

--- db/migrate/20220326161246_add_timestamps_to_content_blocks.decidim.rb
+++ db/migrate/20220326161246_add_timestamps_to_content_blocks.decidim.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 # This migration comes from decidim (originally 20211126183540)
 
 class AddTimestampsToContentBlocks < ActiveRecord::Migration[6.0]
   def up
-    # add_timestamps :decidim_content_blocks, default: Time.zone.now
+    add_timestamps :decidim_content_blocks, default: Time.zone.now
     change_column_default :decidim_content_blocks, :created_at, nil
     change_column_default :decidim_content_blocks, :updated_at, nil
   end
